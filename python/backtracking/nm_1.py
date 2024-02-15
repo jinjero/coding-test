69369d30da54107c4c1c20bfb61b86222cc7a1c5
jinjero <jintojero@gmail.com>
2024-02-16 03:12:44 +0900

    📖 Backjoon 15649

--- python/backtracking/nm_1.py
+++ python/backtracking/nm_1.py
@@ -1,20 +1,20 @@
 import sys
 
 def select_m():
-    if len(s) == m:
-        print(' '.join(map(str, s)))
+    if len(num) == m:
+        print(' '.join(map(str, num)))
         return
     for i in range(1, n+1):
         if visited[i]:
             continue
         visited[i] = True
-        s.append(i)
+        num.append(i)
         select_m()
-        s.pop()
+        num.pop()
         visited[i] = False
 
 n, m = map(int ,sys.stdin.readline().split())
-s = []
+num = []
 visited = [False] * (n+1)
 
 select_m()
